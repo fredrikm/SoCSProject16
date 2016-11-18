d9cd6a3f0d94aaa7dba7cd4e3659ac56c641af71
rasake <rasake@student.chalmers.se>
2016-11-18 22:34:54 +0100

    Add some comments to explain unit tests

--- code/neural_network.py
+++ code/neural_network.py
@@ -32,6 +32,12 @@ class ANN(object):
 class TestANN(unittest.TestCase):
 
     def test_immutability(self):
+        """ This test checks that the ANN instance does not keep a reference to
+            the list that was originally fed to the constructor. We want to ensure
+            that this is not the case, since that would give indirect 
+            access to the ann._weight_matrices property, meaning the object could 
+            easily be corrupted"""
+        
         original_value = 1.5
         simple_matrix = np.ones([2,2]) * original_value
         lst_of_matrices = [simple_matrix, simple_matrix]
@@ -39,7 +45,9 @@ class TestANN(unittest.TestCase):
         simple_matrix[0][0] = -1
         self.assertEqual(ann._weight_matrices[0][0][0], original_value)
 
-    def test_feed_forward(self):
+    def test_feed_forward_vector_ouput(self):
+        """ Tests that the feed_forward executes correctly when the expected 
+            output is a two-dimensional array (two ouput nodes)."""
         simple_matrix = np.zeros([2,2])
         simple_matrix[0][0] = 1
         lst_of_matrices = [simple_matrix, simple_matrix]
@@ -51,6 +59,8 @@ class TestANN(unittest.TestCase):
         self.assertTrue(abs(sum(ann_output-expected_output)) < 0.01)
 
     def test_feed_forward_single_output(self):
+        """ Tests that the feed_forward executes correctly when the expected 
+            output is a one-dimensional array (single output node)."""
         simple_matrix = np.zeros([2,2])
         simple_matrix[0][0] = 1
         lst_of_matrices = [simple_matrix, np.array([1,1])]
@@ -63,6 +73,8 @@ class TestANN(unittest.TestCase):
 
 
     def test_feed_forward_check_formatting1(self):
+        """ Tests that the check_formatting logic does not halt execution when
+            feed_forward is given correctly formatted input"""
         simple_matrix = np.zeros([2,2])
         simple_matrix[0][0] = 1
         lst_of_matrices = [simple_matrix, np.array([1,1])]
@@ -74,6 +86,8 @@ class TestANN(unittest.TestCase):
         self.assertTrue(abs(sum(ann_output-expected_output)) < 0.01)
 
     def test_feed_forward_check_formatting2(self):
+        """ Tests that the check_formatting logic raises AssertionError
+            when feed_forward is given incorrectly formatted input"""        
         simple_matrix = np.zeros([2,2])
         simple_matrix[0][0] = 1
         lst_of_matrices = [simple_matrix, np.array([1,1])]

878bec39c1bedf2b262fc9f0d63962d1263c9b9c
rasake <rasake@student.chalmers.se>
2016-11-19 16:44:11 +0100

    New structure for sensor module

--- code/sensor_module.py
+++ code/sensor_module.py
@@ -8,27 +8,37 @@ import numpy as np
 from main import Environment
 
 
+class RetinaCell(object):
+    def __init__(self):
+        self.agents_in_sight = []
+    def read():
+        if self.agents_in_sight == []:
+            return 0
+        else:
+            return 1   
+    def reset():
+        self.agents_in_sight = []
 
-class RetinaSensor(object):
-    def __init__(self, environment, agent):
+
+class RetinaSensor(object): # Looks for fish
+    def __init__(self, environment, agent, nbr_cells):
         self.environment = environment
         self.agent = agent
-
-    def scan_surroundings(self, radius, field_of_view = -1):
-        #pseudo code        
-        for fish in agent.visible_fish:
-            self.retina_sector.add_fish(fish)
-        for predator in agent.visible_predators:
-            self.retina_sector.add_predator(predator)
-        return [retina_sector.calculate_output() for self.sector_generator]
         
-        
-class FishSensor(VisibilitySensor):
-    def __init__(self):
-        pass
-        
-
-class PredaotorSensor(VisiblitySensor):
-    def __init__(self):
-        pass
-"""
+    def read_fish(self, radius, field_of_view = -1):
+        #pseudo code
+        for cell in self.retina_cells:
+            cell.reset()
+        for fish in self.agent.visible_fish:
+            cell = find_cell(fish)
+            cell.agents_in_sight.append(fish)
+        return [cell.res() for cell in self.retina_cells]
+    
+    def read_predators(self, radius, field_of_view = -1):
+        #pseudo code
+        for cell in self.retina_cells:
+            cell.reset()
+        for predator in self.agent.visible_predators:
+            cell = find_cell(predator)
+            cell.agents_in_sight.append(predator)
+        return [cell.res() for cell in self.retina_cells]

8c09b2042a7af26f7cafc5afc89747325f062148
Fredrik Mäkeläinen <fredrik.makelainen@gmail.com>
2016-11-22 19:44:21 +0100

    Moved to centralised configuration. Cleaned up a bit.

---
+++ code/environment_module.py
@@ -0,0 +1,86 @@
+import random
+import numpy as np
+import pyglet
+
+from agents import Fish, Predator
+from sensor_module import RetinaSensor
+from fsm_module import Predator_FSM
+from neural_network import ANN
+import math_utility_module as mu
+
+# Configuration settings
+class ConfigurationSettings(object):
+    def __init__(self):
+        self.window_width = -1
+        self.window_height = -1
+        self.nbr_fishes = -1
+        self.nbr_predators = -1
+        self.graphics_on = False
+
+        # fish parameters
+        self.fish_sprite_scale = -1
+        self.fish_nbr_retina_cells = -1
+        self.fish_neighbourhood_radius = -1
+        self.fish_speed = 0
+
+        # predator parameters
+        self.predator_sprite_scale = -1
+        self.predator_speed = 0
+
+   
+
+# The simulation environment
+class Environment(object):
+
+    def __init__(self, config_settings, ann_weights):
+        ann = ANN(ann_weights)
+
+        # Configuration
+        self.settings = config_settings
+
+        # We derive boundaries from window width/height
+        self.boundaries = [0, self.settings.window_width, 0, self.settings.window_height]
+
+        #fsm = Predator_FSM(self)
+
+        if self.settings.graphics_on:
+            # sprites
+            fish_image = pyglet.resource.image("orange_fish.png")
+            predator_image = pyglet.resource.image("shark.jpg")
+            self.dead_fish_image = pyglet.resource.image("dead_fish.png")
+            self.center_image(fish_image)
+            self.center_image(predator_image)
+            self.center_image(self.dead_fish_image)
+            # sprite batches for performance
+            self.sprite_batch_fishes = pyglet.graphics.Batch()
+            self.sprite_batch_predators = pyglet.graphics.Batch()
+
+        else:
+            fish_image = None
+            self.sprite_batch_fishes = None
+            predator_image = None
+            self.sprite_batch_predators = None
+        
+        # Initialize fishes
+        self.fish_lst = []
+        for i in range(self.settings.nbr_fishes):
+            pos = np.array([np.random.random() * self.boundaries[1], np.random.random() * self.boundaries[1]])
+            velocity = mu.normalize(np.array([random.randint(-100,100), random.randint(-100,100)]))
+
+            fish = Fish(pos, velocity, i, self, ann, fish_image, self.sprite_batch_fishes)
+            self.fish_lst.append(fish)
+
+
+        # Initialize predators
+        position = np.array([200.0,200.0])
+        velocity = np.array([0.1,0.1])
+        self.predator_lst = [Predator(position, velocity, i, self, predator_image, self.sprite_batch_predators) for i in range(self.settings.nbr_predators)]
+               
+    
+    def __str__(self):
+        return "Environment object"
+
+    # centers an image so it's rotation is around it's center
+    def center_image(self, image):
+        image.anchor_x = image.width / 2
+        image.anchor_y = image.height / 2
--- code/main.py
+++ code/main.py
@@ -6,98 +6,36 @@ Created on Wed Nov 16 15:35:18 2016
 """
 
 import numpy as np
-from time import sleep
 import pyglet
-import random
 
-from agents import Fish, Predator
-from neural_network import ANN
-from sensor_module import RetinaSensor
-from fsm_module import Predator_FSM
-import math_utility_module as mu
+from environment_module import Environment, ConfigurationSettings
 
-
-def simulate(nbr_fish, nbr_predators, boundaries, ann_weights, delta_t = 0.1, nbr_of_iteraions = -1):
-    # ann_weights is a list of matrices  ann_weights = [matrix_layer1, matrix_layer2,...]
-    
-    environment = Environment(nbr_fish, nbr_predators, boundaries, ann_weights)
-    i = 0
-    while nbr_of_iteraions ==-1 or i<nbr_of_iteraions:
-        for fish in environment.fish_lst:
-            fish.think()
-            # placeholder for predator eating fish
-            if sum(fish.sensor.read_predators()) != 0:
-                environment.fish_lst.remove(fish)
-        for predator in environment.predator_lst:
-            predator.think()
-        for fish in environment.fish_lst:
-            fish.advance(delta_t)
-        for predator in environment.predator_lst:
-            predator.advance(delta_t)
-        
-        i += 1
-        #print(str(len(environment.fish_lst)) + " fish in simulation")
-    
-    #TODO calculate fitness score
-    fitness = len(environment.fish_lst)/nbr_fish
-    return fitness
-        
-
-class Environment(object):
-
-    #sprite_batch_fishes = pyglet.graphics.Batch()
-
-    def __init__(self, nbr_fish, nbr_predators, boundaries, ann_weights, graphics_on = False):
-        ann = ANN(ann_weights)
-        #fsm = Predator_FSM(self)
-        position = np.array([200.0,200.0])
-        velocity = np.array([0.1,0.1])
-
-        if graphics_on:
-            # sprites
-            fish_image = pyglet.resource.image("orange_fish.png")
-            predator_image = pyglet.resource.image("shark.jpg")
-            self.dead_fish_image = pyglet.resource.image("dead_fish.png")
-            self.center_image(fish_image)
-            self.center_image(predator_image)
-            self.center_image(self.dead_fish_image)
-            # sprite batches for performance
-            self.sprite_batch_fishes = pyglet.graphics.Batch()
-            self.sprite_batch_predators = pyglet.graphics.Batch()
-
-        else:
-            fish_image = None
-            self.sprite_batch_fishes = None
-            predator_image = None
-            self.sprite_batch_predators = None
-        
-        # Initialize fishes
-        self.fish_lst = []
-        for i in range(nbr_fish):
-            pos = np.array([np.random.random() * boundaries[1], np.random.random() * boundaries[1]])
-            velocity = mu.normalize(np.array([random.randint(-100,100), random.randint(-100,100)]))
-
-            fish = Fish(pos, velocity, i, self, ann, fish_image, self.sprite_batch_fishes)
-            self.fish_lst.append(fish)
-
-
-        #self.fish_lst = [Fish(position, velocity, i, self, sensor, ann) for i in range(nbr_fish)] # Becomes for loop if we wanr to use different ANN variants for different fish        
-        self.predator_lst = [Predator(position, velocity, i, self, predator_image, self.sprite_batch_predators) for i in range(nbr_predators)]
-        
-        self.boundaries = boundaries
-    
-    def __str__(self):
-        return "Environment object"
-
-    # centers an image so it's rotation is around it's center
-    def center_image(self, image):
-        image.anchor_x = image.width / 2
-        image.anchor_y = image.height / 2
-
-
+# Entrypoint for presentation
 if __name__ == "__main__":
-    # Create main window, we hardcode size for now
-    window = pyglet.window.Window(width = 800, height = 600, vsync = False)
+
+    # settings
+    settings = ConfigurationSettings()
+    
+    # simulation settings
+    settings.window_width = 800     # Also used as our simulation boundary
+    settings.window_height = 600    # Also used as our simulation boundary
+    settings.nbr_fishes = 50
+    settings.nbr_predators = 1
+
+    settings.fish_sprite_scale = 0.5
+    settings.fish_nbr_retina_cells = 4
+    settings.fish_neighbourhood_radius = 100
+    settings.fish_speed = 20 # units per second in direction of velocity
+
+    settings.predator_speed = 40
+
+    # graphic settings
+    settings.graphics_on = True
+    settings.fish_sprite_scale = 0.5
+    settings.predator_sprite_scale = 0.6
+        
+    # Create main window
+    window = pyglet.window.Window(width = settings.window_width, height = settings.window_height, vsync = False)
 
     # Set resource path for pyglet
     pyglet.resource.path = ['./textures']
@@ -105,12 +43,10 @@ if __name__ == "__main__":
 
     fps_display = pyglet.clock.ClockDisplay()
 
-    nbr_fish = 50
-    nbr_predators = 1
-    boundaries = [0, window.width, 0, window.height]
     ann_weights = [np.ones([4,8]), np.ones([1,4])]
 
-    environment = Environment(nbr_fish, nbr_predators, boundaries, ann_weights, graphics_on = True)
+    # Instantiate our simulation environment
+    environment = Environment(settings, ann_weights)
 
     # runs once per frame
     def update(dt):
@@ -122,8 +58,9 @@ if __name__ == "__main__":
             #    fish.sprite.image = environment.dead_fish_image
             
             # demonstrate sensor functionality
-            if sum(fish.sensor.read_predators()) != 0 and fish.sprite.image != environment.dead_fish_image:
-                fish.sprite.image = environment.dead_fish_image
+            if environment.settings.graphics_on:
+                if sum(fish.sensor.read_predators()) != 0 and fish.sprite.image != environment.dead_fish_image:
+                    fish.sprite.image = environment.dead_fish_image
                 
         for predator in environment.predator_lst:
             predator.think()
@@ -132,29 +69,19 @@ if __name__ == "__main__":
         for predator in environment.predator_lst:
             predator.advance(dt)
        
-    # event when renderig is requested
+    # event when rendering is requested
     @window.event
     def on_draw():
         window.clear()
         
-        # render the spritebatch containing fishes
-        environment.sprite_batch_fishes.draw()
-        environment.sprite_batch_predators.draw()
+        # render spritebatches
+        if environment.settings.graphics_on:
+            environment.sprite_batch_fishes.draw()
+            environment.sprite_batch_predators.draw()
 
-
-        fps_display.draw()
-        
-    def present(environment):
-        print(environment)
+            fps_display.draw()
 
     pyglet.clock.schedule_interval(update, 1/120.0)
-    pyglet.app.run()
-   
-    """
-    ann_weights = [0.3*np.ones([4,8]), 0.5*np.ones([1,4])]
-    fitness = simulate(50,1,[0,10,0,10], ann_weights, delta_t = 0.3, nbr_of_iteraions = 1000)
-    print(fitness)
-    """
-    
+    pyglet.app.run()    
     
   
